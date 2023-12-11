029c845861bbc36e762658c61332eded006cfe08
KushalK04 <Kushal06Kumar@gmail.com>
2023-12-10 21:21:31 -0800

    pattycakes

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,7 +19,7 @@ export default function Home() {
         <section className={styles.mainCont}>
           <div className={styles.welcomeHome}>
             <h1 className={`${styles.welcome} animated-text`}>
-              WELCOME TO
+              Patty cakes
             </h1>
             <h1 className={`${styles.thrive} animated-text`}>
               THRIVE
